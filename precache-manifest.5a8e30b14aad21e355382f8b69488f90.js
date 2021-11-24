8b0925576e371cf1a0ca2615df2e76519192ccd7
m0hit04 <msrathore0481@gmail.com>
2021-11-24 18:21:49 +0530

    Updates

---
+++ precache-manifest.5a8e30b14aad21e355382f8b69488f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85aaf4eb2bcec1939ceb56283a44c097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f0d77c1038eb9012388",
+    "url": "/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0d77c1038eb9012388",
+    "url": "/static/js/main.25b2bb4e.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
